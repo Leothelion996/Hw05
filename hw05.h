cf97bd789123eeb74535c2d66a522087270714d7
Leo-Win10 <leonardo.herrera1996@yahoo.com>
2024-03-14 20:31:22 -0700

    added the remaining files needed for debug. Added header file.

---
+++ hw05.h
@@ -0,0 +1,11 @@
+/***********************************************************
+∗ Leonardo Herrera ∗ CS1B TTh 7:30 − 9:40pm ∗ Assignemnt 1 ∗
+∗ hw05 ∗ Due : Sunday , March 17 , 2024                    ∗
+************************************************************/
+
+#ifndef _H_
+#define _H_
+
+#include <iostream>
+
+#endif
